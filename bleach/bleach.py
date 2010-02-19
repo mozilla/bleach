518a0a24ae084c734930167cef79f5a6250f2db1
James Socol <james@mozilla.com>
2010-02-18 17:11:00 -0800

    Using html5lib, passes 8/8

--- bleach/bleach.py
+++ bleach/bleach.py
@@ -1,4 +1,42 @@
+from copy import copy
 
+import html5lib
+from html5lib import sanitizer
 
-def clean(string):
-    return string
+ALLOWED_TAGS = [
+    'a',
+    'abbr',
+    'acronym',
+    'b',
+    'blockquote',
+    'code',
+    'em',
+    'i',
+    'li',
+    'ol',
+    'strong',
+    'ul',
+]
+
+ALLOWED_ATTRIBUTES = [
+    'href',
+    'title',
+]
+
+def clean(string, allowed_tags=None, allowed_attributes=None):
+    """Clean an HTML string and return it"""
+
+    if allowed_tags is None:
+        allowed_tags = ALLOWED_TAGS
+
+    if allowed_attributes is None:
+        allowed_attributes = ALLOWED_ATTRIBUTES
+
+    s = copy(sanitizer.HTMLSanitizer)
+
+    s.allowed_elements = allowed_tags
+    s.allowed_attributes = allowed_attributes
+
+    parser = html5lib.HTMLParser(tokenizer=s)
+
+    return parser.parseFragment(string).toxml()
