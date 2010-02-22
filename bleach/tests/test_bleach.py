d0e3a8f221e6d79ba6440d3051cdabee8b0a8e7d
Jeff Balogh <me@jeffbalogh.org>
2010-02-22 13:14:29 -0800

    sanity check: make sure an explicit link doesn't get linkified as well

--- bleach/tests/test_bleach.py
+++ bleach/tests/test_bleach.py
@@ -8,3 +8,10 @@ bl = Bleach()
 def test_bleach_shortcut():
     eq_('&lt; escaped and <a href="http://linkified.com" rel="nofollow">linkified.com</a>',
         bl.bleach('< escaped and linkified.com'))
+
+
+def test_bleach_with_href():
+    """Make sure an explicit link doesn't get linkified as well."""
+    eq_(u'<a href="http://xx.com" title="xx">xx</a> '
+        u'<a href="http://yy.com" rel="nofollow">http://yy.com</a>',
+        bl.bleach('<a title="xx" href="http://xx.com">xx</a> http://yy.com'))
