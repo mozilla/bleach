055c05e7f84459e39100c4fcbebc424e3db52c9b
James Socol <james@mozilla.com>
2012-02-17 14:57:42 -0500

    Version bump with fix.

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='bleach',
-    version='1.1.0',
+    version='1.1.1',
     description='An easy whitelist-based HTML-sanitizing tool.',
     long_description=open('README.rst').read(),
     author='James Socol',
     author_email='james@mozilla.com',
     url='http://github.com/jsocol/bleach',
     license='BSD',
-    packages=['bleach'],
+    packages=find_packages(),
     include_package_data=True,
     package_data={'': ['README.rst']},
     zip_safe=False,
-    install_requires=['html5lib'],
+    install_requires=['html5lib>=0.95'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
