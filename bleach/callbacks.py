550a6fabcfeea2805c8ee3cf702485a4cc356bc0
Marc DM <marcdm@fraser>
2013-06-18 15:59:38 -0500

    lessons learned from the __future__

--- bleach/callbacks.py
+++ bleach/callbacks.py
@@ -1,23 +1,20 @@
 """A set of basic callbacks for bleach.linkify."""
-import six
-import re
+from __future__ import unicode_literals
 
-nfre = re.compile(r'^.*?(\b|\s)(nofollow)(\b|(\s.*))?$', re.U|re.I)
 
 def nofollow(attrs, new=False):
     if attrs['href'].startswith('mailto:'):
         return attrs
-    rel = attrs.get('rel', six.u(''))
-    if rel :
-        if not nfre.match(rel):
-            attrs['rel'] = six.u(' ').join([rel, six.u('nofollow')])
-    else : 
-        attrs['rel'] = six.u('nofollow')
+    rel = [x for x in attrs.get('rel', '').split(' ') if x]
+    if not 'nofollow' in [x.lower() for x in rel]:
+        rel.append('nofollow')
+    attrs['rel'] = ' '.join(rel)
+
     return attrs
 
 
 def target_blank(attrs, new=False):
     if attrs['href'].startswith('mailto:'):
         return attrs
-    attrs['target'] = six.u('_blank')
+    attrs['target'] = '_blank'
     return attrs
--- bleach/sanitizer.py
+++ bleach/sanitizer.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import re
 from xml.sax.saxutils import escape, unescape
 
