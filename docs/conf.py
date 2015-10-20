6d7d5d7af35e5b34170636d85ed597dab30fa5fb
Will Kahn-Greene <willkg@mozilla.com>
2015-10-20 10:01:20 -0400

    Fix copyright years

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Bleach'
-copyright = u'2012-2104, James Socol; 2015, Mozilla Foundation'
+copyright = u'2012-2015, James Socol; 2015, Mozilla Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
