94e2ed83932e12cd5e33176e3e2f398cb3dbd9f0
Will Kahn-Greene <willkg@mozilla.com>
2016-11-05 16:56:44 -0400

    Fix sidebar in docs
    The alabaster theme requires you set the html_sidebar such that the
    navigation works right. This sets that up.

--- docs/conf.py
+++ docs/conf.py
@@ -134,7 +134,14 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+    ]
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
