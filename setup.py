e90b85e3c0c8b0019bfcc5042d43c9d937dd0600
Hugo <hugovk@users.noreply.github.com>
2017-12-18 15:06:26 +0200

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
     include_package_data=True,
     package_data={'': ['README.rst']},
     zip_safe=False,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     install_requires=install_requires,
     setup_requires=setup_requires,
     tests_require=tests_require,
