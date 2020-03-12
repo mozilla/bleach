c4da1c25de9d5d25f646f924f48d33431a199c3f
Aaron Hall <aaronchallmba@gmail.com>
2020-03-12 18:10:26 -0400

    import ABCs from collections.abc for Python 3.9

--- bleach/_vendor/html5lib/_trie/_base.py
+++ bleach/_vendor/html5lib/_trie/_base.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, unicode_literals
 
-from collections import Mapping
+from collections.abc import Mapping
 
 
 class Trie(Mapping):
--- bleach/_vendor/html5lib/treebuilders/dom.py
+++ bleach/_vendor/html5lib/treebuilders/dom.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, unicode_literals
 
 
-from collections import MutableMapping
+from collections.abc import MutableMapping
 from xml.dom import minidom, Node
 import weakref
 
