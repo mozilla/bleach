1f41fcbe782a9e79f50d91b62306a8bc950cf41d
zzj <29055749+zjzh@users.noreply.github.com>
2022-01-21 16:51:23 +0800

    Update _ihatexml.py
    refactoring code with For Else which is more pythonic, concise and efficient

--- bleach/_vendor/html5lib/_ihatexml.py
+++ bleach/_vendor/html5lib/_ihatexml.py
@@ -104,19 +104,18 @@ def charStringToList(chars):
     charRanges = [item.strip() for item in chars.split(" | ")]
     rv = []
     for item in charRanges:
-        foundMatch = False
+        
         for regexp in (reChar, reCharRange):
-            match = regexp.match(item)
-            if match is not None:
-                rv.append([hexToInt(item) for item in match.groups()])
-                if len(rv[-1]) == 1:
-                    rv[-1] = rv[-1] * 2
-                foundMatch = True
-                break
-        if not foundMatch:
+          match = regexp.match(item)
+          if match is not None:
+              rv.append([hexToInt(item) for item in match.groups()])
+              if len(rv[-1]) == 1:
+                  rv[-1] = rv[-1] * 2
+              break
+        else:
             assert len(item) == 1
-
             rv.append([ord(item)] * 2)
+        
     rv = normaliseCharList(rv)
     return rv
 
