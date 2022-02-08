498af1383d89f91e19415ad31eac7097a5dbbafd
Will Kahn-Greene <willkg@mozilla.com>
2022-02-07 22:11:42 -0500

    Fix pip-tools problem and rework requirements (#636)
    This fixes the problem with pip-tools < 6.5.0 and pip 22 by adding
    pip-tools to the dev requirements, setting it to 6.5.0, and installing
    that before checking the requirements.
    
    This also moves requirements checking code to the scripts/run_tests.sh
    script so it's easier to do in a local dev environment.
    
    This also rebuilds the dev requirements.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -11,17 +11,31 @@ MODE=${1:-test}
 
 case "${MODE}" in
   test)
-    pytest ;;
+    pytest
+    ;;
   lint)
-    flake8 bleach/ ;;
+    flake8 bleach/
+    ;;
   vendorverify)
-    ./scripts/vendor_verify.sh ;;
+    ./scripts/vendor_verify.sh
+    ;;
   docs)
-    tox -e docs ;;
+    tox -e docs
+    ;;
   format)
-    black --target-version=py36 bleach/*.py tests/ tests_website/ ;;
+    black --target-version=py36 bleach/*.py tests/ tests_website/
+    ;;
   format-check)
-    black --target-version=py36 --check --diff bleach/*.py tests/ tests_website/ ;;
+    black --target-version=py36 --check --diff bleach/*.py tests/ tests_website/
+    ;;
+  check-reqs)
+    mv requirements-dev.txt requirements-dev.txt.orig
+    pip-compile --generate-hashes requirements-dev.in
+    echo "diffing requirements-dev.txt and requirements-dev.txt.orig"
+    diff requirements-dev.txt requirements-dev.txt.orig
+    rm requirements-dev.txt
+    mv requirements-dev.txt.orig requirements-dev.txt
+    ;;
   *)
     echo "Unknown mode $MODE."
     exit 1
