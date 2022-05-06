153ff84f7b28e6a751636b2b34511598d95a6d09
Lars Eggert <lars@eggert.org>
2022-05-06 08:45:30 +0300

    Only URL-encide the "local-part"

--- bleach/linkifier.py
+++ bleach/linkifier.py
@@ -300,10 +300,15 @@ class LinkifyFilter(html5lib_shim.Filter):
                             {"type": "Characters", "data": text[end : match.start()]}
                         )
 
+                    # URL-encode the "local-part" according to RFC6068
+                    parts = match.group(0).split("@")
+                    parts[0] = quote(parts[0])
+                    address = "@".join(parts)
+
                     # Run attributes through the callbacks to see what we
                     # should do with this match
                     attrs = {
-                        (None, "href"): "mailto:%s" % quote(match.group(0)),
+                        (None, "href"): "mailto:%s" % address),
                         "_text": match.group(0),
                     }
                     attrs = self.apply_callbacks(attrs, True)
