cef9a8e7169bd8d66f625e33306786dcfb7c998b
Will Kahn-Greene <willkg@mozilla.com>
2023-09-18 12:47:34 -0400

    Fix python_requires metadata to be >= 3.8

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     include_package_data=True,
     package_data={"": ["README.rst"]},
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
     classifiers=[
