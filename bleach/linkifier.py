1a41dfc5821ef81dfa7cf3b9b8943484261aa767
NannoSilver <sukhoi47@gmx.net>
2024-08-02 12:38:40 -0300

    Add support for some additonal TLDs at linkifier.py
    Add support for some additional top level domains picked from https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains

--- bleach/linkifier.py
+++ bleach/linkifier.py
@@ -10,19 +10,19 @@ from bleach import html5lib_shim
 DEFAULT_CALLBACKS = [linkify_callbacks.nofollow]
 
 
-TLDS = """ac ad ae aero af ag ai al am an ao aq ar arpa as asia at au aw ax az
-       ba bb bd be bf bg bh bi biz bj bm bn bo br bs bt bv bw by bz ca cat
-       cc cd cf cg ch ci ck cl cm cn co com coop cr cu cv cx cy cz de dj dk
-       dm do dz ec edu ee eg er es et eu fi fj fk fm fo fr ga gb gd ge gf gg
-       gh gi gl gm gn gov gp gq gr gs gt gu gw gy hk hm hn hr ht hu id ie il
+TLDS = """ac ad ae aero af ag ai al am an ao aq ar arpa art as asia at au aw ax az
+       ba bb bd be bf bg bh bi bio biz bj bm bn bo br bs bt bv bw by bz ca cafe cat
+       cc cd cf cg ch ci city ck cl cm cn co com coop cr cu cv cx cy cz de dev dj dk
+       dm do dz ec edu ee eg er es et eu fan fi fj fk fm fo fr ga gb gd ge gf gg
+       gh gi gl gm gn gov gp gq gr gs gt gu guru gw gy hk hm hn hr ht hu id ie il
        im in info int io iq ir is it je jm jo jobs jp ke kg kh ki km kn kp
-       kr kw ky kz la lb lc li lk lr ls lt lu lv ly ma mc md me mg mh mil mk
+       kr kw ky kz la lb lc li link lk lr ls lt lu lv ly ma mc md me mg mh mil mk
        ml mm mn mo mobi mp mq mr ms mt mu museum mv mw mx my mz na name nc ne
        net nf ng ni nl no np nr nu nz om org pa pe pf pg ph pk pl pm pn post
        pr pro ps pt pw py qa re ro rs ru rw sa sb sc sd se sg sh si sj sk sl
-       sm sn so sr ss st su sv sx sy sz tc td tel tf tg th tj tk tl tm tn to
+       sm sn so sr ss st su sv sx sy sz tc td tech tel tf tg th tj tk tl tm tn to top
        tp tr travel tt tv tw tz ua ug uk us uy uz va vc ve vg vi vn vu wf ws
-       xn xxx ye yt yu za zm zw""".split()
+       xn xxx xyz ye yt yu za zm zw""".split()
 
 # Make sure that .com doesn't get matched by .co first
 TLDS.reverse()
